87c61232234b0fb2b744e6b1797bec4063896fcc
Heather Walker <cookiesncream@ladle.net>
2016-03-02 13:32:51 -0800

    added navbar and css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,33 +30,11 @@ class UsersController < ApplicationController
 		user = User.find(session[:user_id])
 		@profile =  user
 		if user.geocoded?
-				@x = User.near(user.address, 10)
-				# @languages = Ninja.select("*").joins(:user).joins(:language).where("user_id=?", User.find(@x.id))
+			@x = User.near(user.address, 10)
+			if @x == []
+				flash[:nobody] = "There are no PairCoders currently in your area."
+			end
 		end
-	
-		# @client = GooglePlaces::Client.new(ENV['google_places_key'])
-		# render :text => ENV['google_places_key']
-		# @client.spots(user.latitude, user.longitude)
-		# render :text => Geocoder.search("library", :location=> {"lat"=> user.latitude, "lng"=>user.longitude})
-		# lang = (user.latitude).to_s
-		# long = (user.longitude).to_s
-		# render :text => Geocoder.search("Starbucks", :bounds => [[user.latitude, user.longitude], [user2.latitude, user2.longitude]])
-		# render :text => Geocoder.spots(:bounds => [[user.latitude, user.longitude], [user2.latitude, user2.longitude]])
-		# render :text => 
-
-
-			# {user.latitude, user.longitude})
-		# @client.spots_by_query('Pizza near Miami Florida')
-		# render :text => @client.spots(-33.8670522, 151.1957362, :types => ['restaurant','food'])
-		# render :text => Hotel.near(”Vancouver, Canada”)
-		# render :text => Location.near([user.latitude, user.longitude], 1)
-		# render :text => Restaurants.near("Rehoboth Beach, DE, US", 50)
-		# Geocoder.search("Paris", :bounds => [[32.1,-95.9], [33.9,-94.3]])
-
-		
-		# Restaurants.near("Rehoboth Beach, DE, US", 50)
-		# Restaurants.near("Rehoboth Beach, DE, US", 50)
-		# Place.near("statue of liberty").where(tags: [:hotel]).all
 	end
 	def edit
 		@profile = User.find(session[:user_id])
